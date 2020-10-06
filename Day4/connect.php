4f70fbdc12061f1a9a3cd7bb1bdab936675cc1d2
Sameerd142 <72194875+Sameerd142@users.noreply.github.com>
2020-10-06 11:46:38 +0530

    first committ

---
+++ Day4/connect.php
@@ -0,0 +1,8 @@
+<?php
+
+//error_reporting(0);
+$conn =mysqli_connect("localhost","root","","result") or die (mysql_connect_error());
+//mysqli_select_db("result") or (mysql_error());
+
+echo "connected!";
+?>
